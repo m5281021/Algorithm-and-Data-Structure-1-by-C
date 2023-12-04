61d7708476d04c0573d7db10e789328641933c09
s1290216 <s1290216@u-aizu.ac.jp>
2023-12-04 10:58:41 +0900

    This is init.

---
+++ 1/A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionSort(int *list, int n) {
+    for(int i = 1; i < n; i++) {
+        int v = list[i];
+        int j = i - 1;
+        while(j >= 0 && list[j] > v) {
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = v;
+        for(int j = 0; j < n; j++) {
+            printf("%d", list[j]);
+            if(j != n - 1) printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    int list[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &list[i]);
+    }
+    insertionSort(list, n);
+}
